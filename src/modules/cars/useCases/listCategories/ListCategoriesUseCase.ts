e8b60c72a3b86d54497b0c5d0f144e4723fc90be
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-16 14:06:51 -0300

    Added UseCases Refactor Project Added Singleton Pattern

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,11 @@
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  execute() {
+    const categories = this.categoriesRepository.list()
+    return categories
+  }
+}
+
+export { ListCategoriesUseCase }
