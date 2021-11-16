ad9c32405ece52e9ad649e072daa4387d4413f82
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-16 14:18:55 -0300

    Added Specifications UseCase

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,10 @@
 import { Router } from 'express'
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository'
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
 
-const specifcationsRoutes = Router()
+const specificationsRoutes = Router()
 
-const specificationsRepository = new SpecificationsRepository()
-
-specifcationsRoutes.post('/', (request, response) => {
-  const { name, description } = request.body
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  )
-
-  createSpecificationService.execute({ name, description })
-
-  return response.status(201).send()
+specificationsRoutes.post('/', (request, response) => {
+  return createSpecificationController.handle(request, response)
 })
 
-export { specifcationsRoutes }
+export { specificationsRoutes }
