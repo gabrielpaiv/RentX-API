72b1afabc0e97a23e636afb91b7eba50a543713a
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-16 12:39:35 -0300

    Added Specification Model Added ICategoriesRepository

--- src/model/Specification.ts
+++ src/model/Specification.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidV4 } from 'uuid'
 
-class Category {
+class Specification {
   id?: string
 
   name: string
@@ -16,4 +16,4 @@ class Category {
   }
 }
 
-export { Category }
+export { Specification }
