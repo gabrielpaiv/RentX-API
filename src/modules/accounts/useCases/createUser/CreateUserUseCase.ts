f4e7f3ece7bb0670c761152448f8cd64a4a55818
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-21 21:48:11 -0300

    Deleted username column

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from 'tsyringe'
+import { hash } from 'bcryptjs'
+
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 
@@ -10,16 +12,21 @@ class CreateUserUseCase {
   ) {}
   async execute({
     name,
-    username,
     email,
     password,
     driver_license
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists')
+    }
+
+    const passwordHash = await hash(password, 8)
     await this.usersRepository.create({
       name,
-      username,
       email,
-      password,
+      password: passwordHash,
       driver_license
     })
   }
