a0c099c00d17942a48f2e0ee5244952a5e9815e9
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-21 22:08:59 -0300

    Added User Authentication with JWT

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase'
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { password, email } = request.body
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase)
+
+    const authenticateInfo = await authenticateUserUseCase.execute({
+      email,
+      password
+    })
+
+    return response.json(authenticateInfo)
+  }
+}
+export { AuthenticateUserController }
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,54 @@
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+
+interface IResponse {
+  user: {
+    name: string
+    email: string
+  }
+  token: string
+}
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password incorrect!')
+    }
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    const token = sign({}, 'ccc3fcb2624d2988a93b4c2e2fecfd47', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    const tokenResponse: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    }
+
+    return tokenResponse
+  }
+}
+export { AuthenticateUserUseCase }
