988f8ef8b1d35183bbfe2f433df76cfb50362cb4
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-22 11:44:24 -0300

    Added exception treatment

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
+import 'reflect-metadata'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import swaggerUI from 'swagger-ui-express'
+
 import './database'
 
 import './shared/container'
 
-import swaggerUI from 'swagger-ui-express'
-import express from 'express'
-
 import swaggerFile from './swagger.json'
 
 import { router } from './routes'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
@@ -16,5 +19,18 @@ app.use(express.json())
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.use(router)
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message })
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`
+    })
+  }
+)
 
 app.listen(3333, () => console.log('Server is running!'))
