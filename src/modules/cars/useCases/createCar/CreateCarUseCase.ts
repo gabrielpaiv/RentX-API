ae30f130e158f25cf2f04ff2042702e7bb046330
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-26 14:13:46 -0300

    Added Create Car UseCase

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,4 +1,6 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car'
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
+import { AppError } from '@shared/errors/AppError'
 import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
@@ -11,10 +13,10 @@ interface IRequest {
   category_id: string
 }
 
-@injectable()
+// @injectable()
 class CreateCarUseCase {
   constructor(
-    @inject('CarsRepository')
+    // @inject('CarsRepository')
     private carsRepository: ICarsRepository
   ) {}
   async execute({
@@ -25,8 +27,16 @@ class CreateCarUseCase {
     fine_amount,
     brand,
     category_id
-  }: IRequest): Promise<void> {
-    this.carsRepository.create({
+  }: IRequest): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    )
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists!')
+    }
+
+    const car = await this.carsRepository.create({
       name,
       description,
       daily_rate,
@@ -35,6 +45,7 @@ class CreateCarUseCase {
       brand,
       category_id
     })
+    return car
   }
 }
 
