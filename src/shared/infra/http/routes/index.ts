f54ec7c9a0b493d4612b1a51f8999da458cc0ae4
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-28 10:46:14 -0300

    Added TypeOrm to Entity Car

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { authenticateRoutes } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
+import { carsRoutes } from './car.routes'
 
 const router = Router()
 
@@ -12,6 +13,8 @@ router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationsRoutes)
 
 router.use('/users', usersRoutes)
+
+router.use('/cars', carsRoutes)
 
 router.use(authenticateRoutes)
 
