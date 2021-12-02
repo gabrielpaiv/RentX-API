952d0eec29f9b23b8ec581a7f487c58c94cc43db
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-12-02 14:25:46 -0300

    Created Rentals

--- src/shared/infra/typeorm/migrations/1637503622142-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1637503622142-CreateUsers.ts
@@ -8,7 +8,8 @@ export class CreateUsers1637503622142 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'uuid'
+            type: 'uuid',
+            isPrimary: true
           },
           {
             name: 'name',
