ce6e6053627972415623ed794516d375c43f4177
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-12-06 11:41:09 -0300

    Fixed Rental Devolution

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -3,20 +3,21 @@ import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental'
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository'
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider'
 import { AppError } from '@shared/errors/AppError'
-import { inject } from 'tsyringe'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   id: string
   user_id: string
 }
 
+@injectable()
 class DevolutionRentalUseCase {
   constructor(
     @inject('RentalsRepository')
     private rentalsRepository: IRentalsRepository,
     @inject('CarsRepository')
     private carsRepository: ICarsRepository,
-    @inject('DayJsDateProvider')
+    @inject('DayjsDateProvider')
     private dateProvider: IDateProvider
   ) {}
   async execute({ id, user_id }: IRequest): Promise<Rental> {
